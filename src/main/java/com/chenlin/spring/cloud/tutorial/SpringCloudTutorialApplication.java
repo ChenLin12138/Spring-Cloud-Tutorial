c5867af25da0fd0fb56c628c8e777708576efad6
ChenLin12138 <scorpion_chenlin@163.com>
2019-12-01 22:49:32 +0800

    Project init

---
+++ src/main/java/com/chenlin/spring/cloud/tutorial/SpringCloudTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.chenlin.spring.cloud.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chenlin/spring/cloud/tutorial/SpringCloudTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenlin.spring.cloud.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
